d1199165fc1d7877956004a1380bd831cbddc007
liufashi <liufashi@yuantutech.com>
2022-04-01 18:00:33 +0800

    fix: word

--- modal/db.js
+++ modal/db.js
@@ -1,33 +1,29 @@
-const mysql=require('mysql');
-const sqlInfo = require("../aliyunSql")
-const pool=mysql.createPool({
-    // host:"",
-    // port:3306,
-    // password:'',
-    // database:'',
-    // connectionLimit:10,
-    // user:'root'
-    ...sqlInfo
-})
-
+const mysql = require("mysql");
+const sqlInfo = require("../aliyunSql");
+const pool = mysql.createPool(sqlInfo);
+// host:"",
+// port:3306,
+// password:'',
+// database:'',
+// connectionLimit:10,
+// user:'root'
 const query = (sql, param) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, param, (err, data) => {
-                connection.release();
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(data);
-
-            })
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      connection.query(sql, param, (err, data) => {
+        connection.release();
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(data);
+      });
+    });
+  });
+};
 
 module.exports = query;
